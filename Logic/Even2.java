cf5fee456eb3c68544eb09a04e421f32b2c4077b
Abhi91188 <abhishekmishra102003@gmail.com>
2026-08-09 12:51:10 +0000

    Find out of even number

---
+++ Logic/Even2.java
@@ -0,0 +1,15 @@
+public class Even2
+{
+    public static void main (String a [])
+    {
+        int n =100;
+        int i = 1;
+        for ( i= 1; i<=100; i++)
+        {
+            if( i % 2== 0 ){
+                System.out.println(i);
+            }
+        }
+    }
+}   
+
