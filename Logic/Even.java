df73dd3a3d66638da812c6c53ab0ce7cec818c80
Abhi91188 <abhishekmishra102003@gmail.com>
2026-08-09 12:25:26 +0000

    even java

---
+++ Logic/Even.java
@@ -0,0 +1,15 @@
+//Check wheather a number ia even or odd 
+ public class Even 
+{
+    public static void main (String a[])
+    {
+        int i = 9;
+        if(  i % 2 ==0 ){
+            System.out.println("the number is even ");
+
+        }else{
+            System.out.println("the number is odd");
+        }
+
+    }
+}
