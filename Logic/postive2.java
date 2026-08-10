6334f6f95cc5323efb4f30eade12f064c4f1cc81
Abhi91188 <abhishekmishra102003@gmail.com>
2026-08-10 19:34:39 +0000

    the sum of positive num

---
+++ Logic/postive2.java
@@ -0,0 +1,23 @@
+// sum off positive num 
+
+import java.util.*;
+
+public class postive2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the fist num");
+        int i = sc.nextInt();
+        System.out.println("enter the secin num");
+        int j = sc.nextInt();
+        int sum = 0;
+        for (i=i; i<=j; i++){
+            if (i>0){
+                sum= sum+i;
+
+            }
+        }System.out.println("the sum of positive num "+ "  = "+ sum );
+
+
+    }
+    
+}
